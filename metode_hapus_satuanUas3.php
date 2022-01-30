5a967b6210c294224744a89685307395f2157f57
ahmadrohmanarif <91609280+ahmadrohmanarif@users.noreply.github.com>
2022-01-30 22:25:53 +0700

    metode_hapus_satuanUas3

---
+++ metode_hapus_satuanUas3.php
@@ -0,0 +1,13 @@
+<?php
+include("koneksi.php");
+$query_delete=mysqli_query($koneksi, "delete from satuan where id_satuan='".$_GET['id_satuan']."'");
+if($query_delete) {
+    header("location:tampil_satuan.php");
+}else{
+    echo mysqli_error();
+}
+?>
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
